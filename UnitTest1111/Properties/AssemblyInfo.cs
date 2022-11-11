8893393b75e243a89c0de0608780749e901973db
sharon <sharon@cathaysec.com.tw>
2022-11-12 00:02:54 +0800

    Unit Test 練習

---
+++ UnitTest1111/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest1111")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest1111")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9776432d-b728-4a7d-b402-764d4f9ded06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1111/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest1111
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        private Calculator _calculator = new Calculator();
+
+        [TestMethod]
+        public void Add_positive_integer()
+        {
+            SumShouldBe(1, 2, 3);
+        }
+
+        private void SumShouldBe(int first, int second, int expected)
+        {
+            Assert.AreEqual(expected, _calculator.Add(first, second));
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
